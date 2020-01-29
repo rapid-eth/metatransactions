3e00c19115f76d1234bab1c20d8c93413d7850b0
Joe Bernitt <j.b5286@gmail.com>
2020-01-29 17:06:54 -0500

    con

--- con/MetaProxy.sol
+++ con/MetaProxy.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.13;
 pragma experimental ABIEncoderV2;
 
-import "./Solidity-RLP/RLPReader.sol";
+import "./solidity-rlp/RLPReader.sol";
 
 contract MetaProxy {
     using RLPReader for RLPReader.RLPItem;
